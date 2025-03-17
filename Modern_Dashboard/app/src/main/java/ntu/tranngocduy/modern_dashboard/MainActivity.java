74c78484e1f1a86d9d3dd7f7b1ae4ad4301822cc
Trần Ngọc Duy <duy.tn.63cntt@ntu.edu.vn>
2025-03-18 01:25:49 +0700

    update

--- Modern_Dashboard/app/src/main/java/ntu/tranngocduy/modern_dashboard/MainActivity.java
+++ Modern_Dashboard/app/src/main/java/ntu/tranngocduy/modern_dashboard/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.moderndashboard;
+package ntu.tranngocduy.modern_dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -6,6 +6,9 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import ntu.tranngocduy.modern_dashboard.ClothingActivity;
+import ntu.tranngocduy.modern_dashboard.R;
 
 public class MainActivity extends AppCompatActivity {
 
