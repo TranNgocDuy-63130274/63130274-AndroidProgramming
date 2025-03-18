5d44981d721400bc11c5974551973541ad2c4473
Trần Ngọc Duy <duy.tn.63cntt@ntu.edu.vn>
2025-03-18 07:48:11 +0700

    Update build.gradle.kts

--- Ex6_IntentDonGian/app/build.gradle.kts
+++ Ex6_IntentDonGian/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "til.edu.ex6_intentdongian"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
