bf5defa14f1c7adc6ce028905240aafd5638d17a
Trần Ngọc Duy <duy.tn.63cntt@ntu.edu.vn>
2025-03-28 17:03:39 +0700

    Update
    Recycler View còn bị lỗi đã cung cấp hình ảnh

---
+++ UsingRecyclerView/app/src/main/java/ntu/ngocduy/usingrecyclerview/LandScape.java
@@ -0,0 +1,27 @@
+package ntu.ngocduy.usingrecyclerview;
+
+public class LandScape {
+    String landImageFileName;
+    String landCation;
+
+    public LandScape(String landCation, String landImageFileName) {
+        this.landCation = landCation;
+        this.landImageFileName = landImageFileName;
+    }
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
+}
